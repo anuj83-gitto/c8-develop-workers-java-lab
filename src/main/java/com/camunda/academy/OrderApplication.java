9eb46a2fea3e558433632720ff962973d530fa8e
anuj83-gitto <anoojit.dhar@gmail.com>
2025-06-19 17:02:09 +0200

    Update OrderApplication.java
    updated java

--- src/main/java/com/camunda/academy/OrderApplication.java
+++ src/main/java/com/camunda/academy/OrderApplication.java
@@ -34,10 +34,10 @@ public class OrderApplication {
 
     // Process instance creation
     private static final String PROCESS_ID = "orderProcess";
-    private static final int NUM_INSTANCES = 1; // TOTAL NUMBER OF NEW PROCESS INSTANCES CREATED
+    private static final int NUM_INSTANCES = 3; // TOTAL NUMBER OF NEW PROCESS INSTANCES CREATED
 
     // Worker configuration
-   // private static final int WORKER_TIMEOUT = 1; // Set the time for how long a job is exclusively assigned for this worker.
+    private static final int WORKER_TIMEOUT = 20; // Set the time for how long a job is exclusively assigned for this worker.
         
     public static void main(String[] args) {
         loadProperties();
